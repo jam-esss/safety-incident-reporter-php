619035737bba539c5dba065e9c5384b19a6b14a9
James <76698733+jam-esss@users.noreply.github.com>
2026-05-01 17:12:08 +0100

    Preparing middleware for client routes

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -10,6 +10,6 @@ $router->group('/', function (RouteGroup $router) {
     include routes_dir("public.php");
 });
 
-$router->group('/client', function (RouteGroup $router) {
+$router->group('/client', function (RouteGroup $router) { //$group
     include routes_dir("client.php");
-});
+});//->middleware(new \app\Middleware\LoginMiddleware::class) middleware will limit client page to logged in users;
