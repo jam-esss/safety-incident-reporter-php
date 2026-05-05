32d729e7692eade9e14adb0317079dadeda0dd63
James <76698733+jam-esss@users.noreply.github.com>
2026-05-05 21:43:36 +0100

    stopped bootstrap validator preventing post / made it check for success message.

--- public/scripts/BootstrapFormValidator.js
+++ public/scripts/BootstrapFormValidator.js
@@ -1,20 +1,22 @@
 const forms = document.querySelectorAll('form');
 const successMessage = document.getElementById('successMessage');
 
-
 forms.forEach((form) => {
     form.addEventListener('submit', function (event) {
-        // Prevent default submission and add 'was-validated' class.
-        event.preventDefault();
+
         form.classList.add('was-validated');
 
-        // If form is not valid, stop here and let Bootstrap show errors.
         if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
             return;
         }
 
-        // If form is valid, hide the form and display success message.
-        form.style.display = 'none';
-        successMessage.style.display = 'block';
+        if (successMessage) {
+            event.preventDefault();
+
+            form.style.display = 'none';
+            successMessage.style.display = 'block';
+        }
     });
-})
+});
