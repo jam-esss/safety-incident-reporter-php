95ef24501f4926b961329608dbbbe1a649e9fdb1
James <76698733+jam-esss@users.noreply.github.com>
2026-05-10 16:54:54 +0100

    updated db helper

--- app/App.php
+++ app/App.php
@@ -21,11 +21,12 @@ class App
     public function getDb(): PDO
     {
         if (is_null($this->db)) {
-            if (!isset($this->env['DB_HOST'], $this->env['DB_NAME'], $this->env['DB_USERNAME'], $this->env['DB_PASSWORD'])) {
+            if (!isset($this->env['DB_HOST'], $this->env['DB_NAME'], $this->env['DB_USER'], $this->env['DB_PASSWORD'])) {
                 throw new RuntimeException("Cannot connect to the database.");
             }
             $this->db = new PDO("mysql:host={$this->env['DB_HOST']};dbname={$this->env['DB_NAME']}", $this->env['DB_USER'], $this->env['DB_PASSWORD']);
         }
+        return $this->db;
     }
 
 }
