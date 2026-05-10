ac0932da0185e40804fee64e20f42dbbd3019d52
James <76698733+jam-esss@users.noreply.github.com>
2026-05-10 16:53:47 +0100

    + updated router

--- public/index.php
+++ public/index.php
@@ -5,13 +5,16 @@ require __DIR__ . '/../vendor/autoload.php';
 ini_set('error_log', __DIR__.'/../errors.log');
 
 use League\Route\Router;
+use League\Route\Strategy\ApplicationStrategy;
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 
 $request = initRequest();
-// Initialise app
+
 app();
+
 $router = new Router();
+$router->setStrategy(new ApplicationStrategy());
 
 require routes_dir("routes.php");
 
