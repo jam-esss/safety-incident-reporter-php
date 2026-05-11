dbf98d65586f59263c85a1c5970dfd960d802d31
James <76698733+jam-esss@users.noreply.github.com>
2026-05-11 16:31:45 +0100

    + include avatar in sql query

--- users.sql
+++ users.sql
@@ -5,9 +5,9 @@ CREATE TABLE `safety-incident-reporter`.`users`
     `sn` VARCHAR(60) NOT NULL COMMENT 'Surname',
     `email` VARCHAR(60) NOT NULL COMMENT 'Email',
     `password` TINYTEXT NOT NULL COMMENT 'Password (Hash)',
+    `avatar` TEXT NULL DEFAULT NULL COMMENT 'Profile Picture',
     `tkn` VARCHAR(12) NOT NULL COMMENT 'Token',
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
     `removed_at` TIMESTAMP NULL DEFAULT NULL COMMENT 'Time Removed',
-
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB COMMENT = 'Users Table';
